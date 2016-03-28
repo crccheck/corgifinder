999f053088952838fe0b19c8310fb1c27a1f8494
crccheck <c@crccheck.com>
2016-03-28 04:29:20 +0100

    add user input

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,14 @@ import corgiNames from './names';
 
 const chat = document.getElementById('chat');
 const name = corgiNames[Math.floor(Math.random() * corgiNames.length)];
-let speakTimer;
+const userInput = document.getElementById('arf')
+const MAX_HISTORY = 100
+const CHATTER_DELAY = 60
+const CHATTER_JITTER = 60
+const REPLY_DELAY = 1
+const REPLY_JITTER = 3
+
+let speakTimer
 
 function greet() {
   const newElem = document.createElement('li');
@@ -15,14 +22,51 @@ function speak() {
   const newElem = document.createElement('li');
   newElem.className = 'message corgi';
   newElem.innerHTML = `<span class="name">${name}</span>: aRf aRf`;
+  // Prevent creating an infinite number of DOM nodes
+  if (chat.childElementCount > MAX_HISTORY) {
+    chat.firstChild.remove()
+  }
   chat.appendChild(newElem);
+  chat.scrollTop = chat.scrollHeight;
+}
+
+function reply(text) {
+  const newElem = document.createElement('li');
+  newElem.className = 'message user';
+  newElem.innerHTML = '<span class="name">You</span>: '
+  // Prevents the user from injecting html
+  newElem.appendChild(document.createTextNode(text))
+  chat.appendChild(newElem);
+  if (speakTimer) {
+    window.clearTimeout(speakTimer)
+  }
+  const speakAgain = REPLY_DELAY + Math.random() * REPLY_JITTER  // seconds
+  window.setTimeout(autoSpeak, speakAgain * 1000)
 }
 
 function autoSpeak() {
+  if (speakTimer) {
+    window.clearTimeout(speakTimer)
+  }
   speak();
-  const speakAgain = 60 + Math.random() * 100;  // seconds
+  const speakAgain = CHATTER_DELAY + Math.random() * CHATTER_JITTER  // seconds
   speakTimer = setTimeout(autoSpeak, speakAgain * 1000);
 }
 
-greet();
-autoSpeak();
+function onKey(e) {
+  switch (e.keyCode) {
+    case 13:
+      const text = userInput.value
+      if (text) {
+        reply(userInput.value)
+      }
+      // falls through
+    case 27:
+      userInput.value = ''
+    break;
+  }
+}
+userInput.addEventListener('keypress', onKey, false)
+
+greet()
+window.setTimeout(autoSpeak, REPLY_DELAY * 1000)
